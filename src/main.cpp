e39cad7bf664e57c60dd965a361cb1a4e58204dd
Fisa06 <karelvrzal26@gmail.com>
2023-11-24 18:43:31 +0100

    1

--- src/main.cpp
+++ src/main.cpp
@@ -82,11 +82,11 @@ void loop()
 volatile int8_t AS3935IsrTrig = 0;
 
 //**************Teplota***********
-//const int TempMain = 4;
-//OneWire oneWireDS(TempMain);
-//DallasTemperature temp(&oneWireDS);
+const int TempMain = 7;
+OneWire oneWireDS(TempMain);
+DallasTemperature temp(&oneWireDS);
 //***********Vlhkost************
-//const int DHTpin = 17;
+const int DHTpin = 17;
 
 //**************UV index*************
 const int UVread = 5;
@@ -97,7 +97,7 @@ DHTesp dht;
 DFRobot_AS3935_I2C  lightning0((uint8_t)IRQ_PIN, (uint8_t)AS3935_I2C_ADDR);
 MQ135 airQuality = MQ135(AirQuality);
 
-Adafruit_SHT31 sht31 = Adafruit_SHT31();
+//Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
 float Readings[5] = {0,0,0,0,0}; //Teplota, Vlhkost, Bouřka[km], UVindex, KvalitaVzduchu, 
 
@@ -111,41 +111,21 @@ void setup()
   String thisBoard= ARDUINO_BOARD;
   Serial.println(thisBoard);
 
-   if (! sht31.begin(0x45))  // alternatively 0x44, depends on ADDR pin
+  temp.begin();
+
+   /* if (! sht31.begin(0x45))  // alternatively 0x44, depends on ADDR pin
   {
     Serial.println("SHT31 not found");
     Serial.println("Check the connection");
     while (1) delay(1);
-  }
+  } */
   // Autodetect is not working reliable, don't use the following line
   // dht.setup(17);
   // use this instead: 
-  //dht.setup(DHTpin, DHTesp::DHT11); // Connect DHT sensor to GPIO 17
+  dht.setup(DHTpin, DHTesp::DHT11); // Connect DHT sensor to GPIO 17
 
 
-  Serial.println("DFRobot AS3935 lightning sensor begin!");
-
-  while (lightning0.begin() != 0){
-    Serial.print(".");
-  }
-  lightning0.defInit();
-
-  #if defined(ESP32) || defined(ESP8266)
-    attachInterrupt(digitalPinToInterrupt(IRQ_PIN),AS3935_ISR,RISING);
-  #else
-    attachInterrupt(/*Interrupt No*/0,AS3935_ISR,RISING);
-  #endif
-
-  // Configure sensor
-  lightning0.manualCal(AS3935_CAPACITANCE, AS3935_MODE, AS3935_DIST);
-  // Enable interrupt (connect IRQ pin IRQ_PIN: 2, default)
-
-//  Connect the IRQ and GND pin to the oscilloscope.
-//  uncomment the following sentences to fine tune the antenna for better performance.
-//  This will dispaly the antenna's resonance frequency/16 on IRQ pin (The resonance frequency will be divided by 16 on this pin)
-//  Tuning AS3935_CAPACITANCE to make the frequency within 500/16 kHz ± 3.5%
-//  lightning0.setLcoFdiv(0);
-//  lightning0.setIRQOutputSource(3);
+ 
 
 }
 
@@ -156,22 +136,12 @@ void loop()
   float humidity = dht.getHumidity();
   float temperature = dht.getTemperature();
 
-  Serial.print(dht.getStatusString());
-  Serial.print("\t");
-  Serial.print(humidity, 1);
-  Serial.print("\t\t");
-  Serial.print(temperature, 1);
-  Serial.print("\t\t");
-  Serial.print(dht.toFahrenheit(temperature), 1);
-  Serial.print("\t\t");
-  Serial.print(dht.computeHeatIndex(temperature, humidity, false), 1);
-  Serial.print("\t\t");
-  Serial.println(dht.computeHeatIndex(dht.toFahrenheit(temperature), humidity, true), 1);
+  
   delay(2000);
 
   Readings[1] = dht.getHumidity();
 
-  // It does nothing until an interrupt is detected on the IRQ pin.
+ /*  // It does nothing until an interrupt is detected on the IRQ pin.
   while (AS3935IsrTrig == 0) {delay(1);}
   delay(5);
 
@@ -198,17 +168,19 @@ void loop()
     Serial.println("Disturber discovered!");
   }else if (intSrc == 3){
     Serial.println("Noise level too high!");
-  }
+  } */
 
   //Teplota
 
-  //temp.requestTemperatures();
-  //Readings[0] = int(temp.getTempCByIndex(0));
-  Readings[0] = sht31.readTemperature();
+  temp.requestTemperatures();
+  Readings[0] = int(temp.getTempCByIndex(0));
+  //Readings[0] = sht31.readTemperature();
+  //Readings[0] = dht.getTemperature();
 
 
   //Vlhkost
-  Readings[1] = sht31.readHumidity();
+  //Readings[1] = sht31.readHumidity();
+  Readings[1] = dht.getHumidity();
 
   //UV
   int UVvalue = analogRead(UVread);
@@ -227,9 +199,14 @@ void loop()
 
 
   Readings[4] = airQuality.getPPM();
+  
+  Serial.println(Readings[0]);
+  Serial.println(Readings[1]);
+  Serial.println(UVvalue);
+  Serial.println(Readings[4]);
 }
 
-void AS3935_ISR()
+/* void AS3935_ISR()
 {
   AS3935IsrTrig = 1;
-}
+} */
